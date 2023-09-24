58609cb6420620fb23decc14d3cad0057b779376
Reza A <87265763+RA19HS@users.noreply.github.com>
2023-09-25 00:33:52 +0330

    Refactor

--- median.cpp
+++ median.cpp
@@ -5,9 +5,7 @@
 
 static constexpr double EPS = 1E-5;
 
-std::vector<int> u, v;
-
-double median(std::vector<int> &v, int l, int r) {
+double median(const std::vector<int> &v, int l, int r) {
 	assert(l < r);
 	if ((r-l) % 2 == 1)
 		return v[(l+r) / 2];
@@ -29,57 +27,60 @@ bool less_than(double a, double b) {
 	return a+EPS < b;
 }
 
-double median(int ul, int ur, int vl, int vr) {
-	double u_median = median(u, ul, ur);
-	double v_median = median(v, vl, vr);
+double median_edge_case_1_element_array(int x, const std::vector<int> &v, int vl, int vr) {
+	const double v_median = median(v, vl, vr);
+	const int vm = (vl+vr) / 2;
+	const bool odd_length_v = (vr-vl) % 2 == 1;
+	if (less_than(x, v_median)) {
+		if (odd_length_v)
+			return (v[vm] + std::max(x, v[vm-1])) / 2.;
+		else
+			return std::max(x, v[vm-1]);
+	}
+	else {
+		if (odd_length_v)
+			return (v[vm] + std::min(x, v[vm+1])) / 2.;
+		else
+			return std::min(x, v[vm]);
+	}
+}
+
+double median_edge_case_2_elements_array(const std::array<int, 2> u, const std::vector<int> &v, int vl, int vr) {
+	const double v_median = median(v, vl, vr);
+	const int vm = (vl+vr) / 2;
+	const bool odd_length_v = (vr-vl) % 2 == 1;
+	if (less_than(u[1], v_median)) {
+		if (odd_length_v)
+			return std::max(u[1], v[vm-1]);
+		else
+			return (v[vm] + std::max(u[1], v[vm-1])) / 2.;
+	}
+	if (less_than(v_median, u[0])) {
+		if (odd_length_v)
+			return std::min(u[0], v[vm+1]);
+		else
+			return (v[vm-1] + std::min(u[0], v[vm])) / 2.;
+	}
+	else {
+		if (odd_length_v)
+			return v[vm];
+		else
+			return (std::max(u[0], v[vm-1]) + std::min(u[1], v[vm])) / 2.;
+	}
+}
+
+double median(const std::vector<int> &u, int ul, int ur, const std::vector<int> &v, int vl, int vr) {
+	assert(size(u) <= size(v));
+	const double u_median = median(u, ul, ur);
+	const double v_median = median(v, vl, vr);
 	if (equal(u_median, v_median))
 		return u_median;
 	if (ur-ul == 1 && vr-vl == 1)
 		return (u[ul]+v[vl]) / 2.;
-	if (vr-vl < 3) {
-		swap(u, v); // It's O(1)
-		std::swap(ul, vl);
-		std::swap(ur, vr);
-		std::swap(u_median, v_median);
-	}
-	if (ur-ul == 1) {
-		const int vm = (vl+vr) / 2;
-		const bool odd_length_v = (vr-vl) % 2 == 1;
-		if (less_than(u[ul], v_median)) {
-			if (odd_length_v)
-				return (v[vm] + std::max(u[ul], v[vm-1])) / 2.;
-			else
-				return std::max(u[ul], v[vm-1]);
-		}
-		else {
-			if (odd_length_v)
-				return (v[vm] + std::min(u[ul], v[vm+1])) / 2.;
-			else
-				return std::min(u[ul], v[vm]);
-		}
-	}
-	if (ur-ul == 2) {
-		const int vm = (vl+vr) / 2;
-		const bool odd_length_v = (vr-vl) % 2 == 1;
-		if (less_than(u[ul+1], v_median)) {
-			if (odd_length_v)
-				return std::max(u[ul+1], v[vm-1]);
-			else
-				return (v[vm] + std::max(u[ul+1], v[vm-1])) / 2.;
-		}
-		if (less_than(v_median, u[ul])) {
-			if (odd_length_v)
-				return std::min(u[ul], v[vm+1]);
-			else
-				return (v[vm-1] + std::min(u[ul], v[vm])) / 2.;
-		}
-		else {
-			if (odd_length_v)
-				return v[vm];
-			else
-				return (std::max(u[ul], v[vm-1]) + std::min(u[ul+1], v[vm])) / 2.;
-		}
-	}
+	if (ur-ul == 1)
+		return median_edge_case_1_element_array(u[ul], v, vl, vr);
+	if (ur-ul == 2)
+		return median_edge_case_2_elements_array({u[ul], u[ul+1]}, v, vl, vr);
 	int removable_length = std::min(nonmedian_half_length(ul, ur), nonmedian_half_length(vl, vr));
 	if (less_than(u_median, v_median)) {
 		ul += removable_length;
@@ -89,27 +90,38 @@ double median(int ul, int ur, int vl, int vr) {
 		vl += removable_length;
 		ur -= removable_length;
 	}
-	return median(ul, ur, vl, vr);
+	return median(u, ul, ur, v, vl, vr);
+}
+
+class VernaTask {
+public:
+	double findMedian(std::vector<int> &nums1, std::vector<int> &nums2) {
+		if (size(nums1) < size(nums2))
+			return median(nums1, 0, size(nums1), nums2, 0, size(nums2));
+		return median(nums2, 0, size(nums2), nums1, 0, size(nums1));
+	}
+};
+
+void test() {
+	VernaTask task3;
+	std::vector<int> u, v;
+	u = {1, 3, 5, 7, 9, 11, 13}; v = {2, 4, 6, 8, 10}; assert(6.5 == task3.findMedian(u, v));
+	u = {1, 3, 5, 7, 9, 11}; v = {2, 4, 6, 8, 10}; assert(6 == task3.findMedian(u, v));
+	u = {1, 3, 5, 7, 9}; v = {2, 4, 7, 8, 10, 20}; assert(7 == task3.findMedian(u, v));
+	u = {1, 3, 5, 6, 9, 11}; v = {2, 4, 8, 8, 10, 20}; assert(7 == task3.findMedian(u, v));
+	u = {1, 3, 5, 6, 9, 11}; v = {4}; assert(5 == task3.findMedian(u, v));
+	u = {1, 3, 5, 6, 9, 11}; v = {4, 7}; assert(5.5 == task3.findMedian(u, v));
+	u = {1, 2}; v = {3}; assert(2 == task3.findMedian(u, v));
+	u = {1, 3}; v = {2}; assert(2 == task3.findMedian(u, v));
+	u = {2, 3}; v = {1}; assert(2 == task3.findMedian(u, v));
+	u = {3, 5, 7}; v = {1}; assert(4 == task3.findMedian(u, v));
+	u = {3, 5, 7}; v = {4}; assert(4.5 == task3.findMedian(u, v));
+	u = {3, 5, 7}; v = {6}; assert(5.5 == task3.findMedian(u, v));
+	u = {3, 5, 7}; v = {8}; assert(6 == task3.findMedian(u, v));
+	u = {3, 5}; v = {1, 2, 7}; assert(3 == task3.findMedian(u, v));
 }
 
 int main() {
-	u = {1, 3, 5, 7, 9, 11, 13};
-	v = {2, 4, 6, 8, 10};
-	std::cerr << "6.5 ? " << median(0, size(u), 0, size(v)) << '\n';
-	u = {1, 3, 5, 7, 9, 11};
-	v = {2, 4, 6, 8, 10};
-	std::cerr << "6 ? " << median(0, size(u), 0, size(v)) << '\n';
-	u = {1, 3, 5, 7, 9};
-	v = {2, 4, 7, 8, 10, 20};
-	std::cerr << "7 ? " << median(0, size(u), 0, size(v)) << '\n';
-	u = {1, 3, 5, 6, 9, 11};
-	v = {2, 4, 8, 8, 10, 20};
-	std::cerr << "7 ? " << median(0, size(u), 0, size(v)) << '\n';
-	u = {1, 3, 5, 6, 9, 11};
-	v = {4};
-	std::cerr << "5 ? " << median(0, size(u), 0, size(v)) << '\n';
-	u = {1, 3, 5, 6, 9, 11};
-	v = {4, 7};
-	std::cerr << "5.5 ? " << median(0, size(u), 0, size(v)) << '\n';
+	test();
 	return 0;
 }
